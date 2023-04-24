6e300453456ded55958b1a71990a4e910aa81ed0
Joshua Marble <jsmarble@gmail.com>
2023-04-24 11:05:02 -0500

    Add demo proj

---
+++ demo/LassoDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Lasso;
+using StackExchange.Redis;
+
+const long MAX_REQ = 5;
+const int LIMIT_WINDOW_SEC = 10;
+
+Console.WriteLine("Hello, this example demonstrates usage of Lasso for managing usage limits!");
+Console.WriteLine("Press [Enter] to request a new GUID be generated and written to the console.");
+Console.WriteLine($"The maximum number of requests is {MAX_REQ} per {LIMIT_WINDOW_SEC} seconds.");
+Console.WriteLine();
+
+var muxer = ConnectionMultiplexer.Connect("10.0.2.12:6379");
+IRedisKeyBuilder keyBuilder = new DailyUtcRedisKeyBuilder();
+IRelativeExpirationStrategy expirationStrategy = new TimeSpanExpirationStrategy(TimeSpan.FromSeconds(LIMIT_WINDOW_SEC), false);
+
+IUsageManager usageManager = new RedisUsageManager(muxer, keyBuilder, expirationStrategy);
+var usage = new UsageRequest
+{
+    Context = "LassoDemo",
+    Resource = "GuidGen",
+    Quota = MAX_REQ
+};
+
+while (true)
+{
+    Console.ReadLine();
+    UsageResult res = await usageManager.IncrementAsync(usage);
+    Console.WriteLine($"[Usage: {res.Current} / {res.Quota}]");
+
+    if (res.Current <= res.Quota)
+        Console.WriteLine(Guid.NewGuid().ToString());
+    else
+        Console.WriteLine("You have exceeded the request limit. Please wait and try again.");
+}
